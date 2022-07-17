be8a9d68a9607a98e129d3544d619b265d51f8d4
oussema <oussema.sferi@gmail.com>
2022-07-17 20:02:30 +0100

    code refactored + checking issue for null results

--- MarkifyScraper.php
+++ MarkifyScraper.php
@@ -1,80 +1,36 @@
 <?php
-
+require './Utils/HelperService.php';
 use Goutte\Client;
-class HelperService
+class MarkifyScraper
 {
     const DOMAIN = 'https://search.ipaustralia.gov.au';
-    private array $element = [];
-    private array $result;
-    public function __construct()
+    public function getData()
     {
-        $this->result = [];
-    }
-    public function scrapeData($crawler)
-    {
-        $output = $crawler->filter('#resultsTable > tbody > tr')->each(function ($node) {
-            //$this->counter++;
-            //$logoUrl = $node->filter('.image > img')->attr('src');
-            $index = $node->filter('.table-index')->text();
-            //echo $index;
-
-            //
-            $number = $node->filter('.number')->text();
-            $this->element["number"]= $number;
-
-            //
-            if($node->filter('.image > img')->count() > 0)
-            {
-                $logoUrl = $node->filter('.image > img')->attr('src');
-            } else {
-                $logoUrl = "none";
-            }
-            $this->element["logo_url"]= $logoUrl;
-
-            $name = $node->filter('.words')->text();
-            $this->element["name"]= $name;
-
-            //
-            $classes = $node->filter('.classes')->text();
-            $this->element["classes"]= $classes;
-
-            //
-            if($node->filter('.status > div > span')->count() > 0)
-            {
-                $status = trim($node->filter('.status > div > span')->text());
-            } else {
-                $status = $node->filter('.status')->html();
-                //$status = $node->filter('.status')->html();
-                $status = trim(preg_replace('~<i(.*?)</i>~Usi', '', $status));
-            }
-            $colonIndex = strpos($status, ':');
-            if($colonIndex !== false)
-            {
-                $statusOne = substr($status, 0, $colonIndex);
-                $statusTwo = trim(substr($status, $colonIndex + 1));
-                $this->element["status1"]= $statusOne;
-                $this->element["status2"]= $statusTwo;
-            } else {
-                $this->element["status1"]= $status;
-                $this->element["status2"]= $status;
-            }
-
-            //
-            $detailsPageUrl = $node->attr('data-markurl');
-            $this->element["details_page_url"]= self::DOMAIN . $detailsPageUrl;
-
-            //
-
-
-            // $status = $node->filter('.status > div > span')->text();
-
-            $this->result[$index] = json_encode($this->element, JSON_UNESCAPED_SLASHES);
-
-            //array_push($this->test, $response);
-            //var_dump($index);
-            // var_dump($this->element["status1"]);
-            //var_dump($this->element["status2"]);
-        });
-        return $this->result;
+        $keyword = readline('Enter Your search keyword: ');
+        if(!$keyword) return;
+        $helper = new HelperService();
+        $client = new Client();
+        $crawler = $client->request('GET', self::DOMAIN . "/trademarks/search/advanced");
+        $form = $crawler->selectButton('Search')->form();
+        $crawler = $client->submit($form, ['wv[0]' => $keyword]);
+        $allResults = $helper->getSinglePageData($crawler);
+        // Search Results Count
+        $helper->getAllResultsCount($crawler);
+        // If results pages are in a single page
+        if($helper->checkIfSinglePage($crawler))
+        {
+            print_r($allResults);
+            return;
+        }
+        // If results pages are in more than one page
+        while(!$helper->checkIfSinglePage($crawler))
+        {
+            // Automate clicking on next page button
+            $link = $crawler->selectLink('Next page')->link();
+            $crawler = $client->click($link);
+            //Appending next page results to the existing results
+            $allResults += $helper->getSinglePageData($crawler);
+        }
+        print_r($allResults);
     }
 }
--- Utils/HelperService.php
+++ Utils/HelperService.php
@@ -1,50 +1,31 @@
 <?php
-
 use Goutte\Client;
 class HelperService
 {
     const DOMAIN = 'https://search.ipaustralia.gov.au';
-    private array $element = [];
-    private array $result;
-    public function __construct()
-    {
-        $this->result = [];
-    }
-    public function scrapeData($crawler)
+    private array $row;
+    private array $singlePageResults;
+    public function getSinglePageData($crawler) : array
     {
         $output = $crawler->filter('#resultsTable > tbody > tr')->each(function ($node) {
-            //$this->counter++;
-            //$logoUrl = $node->filter('.image > img')->attr('src');
+            // Mapping data
             $index = $node->filter('.table-index')->text();
-            //echo $index;
-
             //
             $number = $node->filter('.number')->text();
-            $this->element["number"]= $number;
-
+            $this->row["number"]= $number;
             //
-            if($node->filter('.image > img')->count() > 0)
-            {
-                $logoUrl = $node->filter('.image > img')->attr('src');
-            } else {
-                $logoUrl = "none";
-            }
-            $this->element["logo_url"]= $logoUrl;
-
+            $logoUrl = $node->filter('.image > img')->count() > 0 ? $node->filter('.image > img')->attr('src') : "none";
+            $this->row["logo_url"]= $logoUrl;
             $name = $node->filter('.words')->text();
-            $this->element["name"]= $name;
-
+            $this->row["name"]= $name;
             //
             $classes = $node->filter('.classes')->text();
-            $this->element["classes"]= $classes;
-
+            $this->row["classes"]= $classes;
             //
             if($node->filter('.status > div > span')->count() > 0)
-            {
                 $status = trim($node->filter('.status > div > span')->text());
-            } else {
+            else {
                 $status = $node->filter('.status')->html();
-                //$status = $node->filter('.status')->html();
                 $status = trim(preg_replace('~<i(.*?)</i>~Usi', '', $status));
             }
             $colonIndex = strpos($status, ':');
@@ -52,29 +33,30 @@ class HelperService
             {
                 $statusOne = substr($status, 0, $colonIndex);
                 $statusTwo = trim(substr($status, $colonIndex + 1));
-                $this->element["status1"]= $statusOne;
-                $this->element["status2"]= $statusTwo;
+                $this->row["status1"]= $statusOne;
+                $this->row["status2"]= $statusTwo;
             } else {
-                $this->element["status1"]= $status;
-                $this->element["status2"]= $status;
+                $this->row["status1"]= $status;
+                $this->row["status2"]= $status;
             }
-
             //
             $detailsPageUrl = $node->attr('data-markurl');
-            $this->element["details_page_url"]= self::DOMAIN . $detailsPageUrl;
-
-            //
-
-
-            // $status = $node->filter('.status > div > span')->text();
-
-            $this->result[$index] = json_encode($this->element, JSON_UNESCAPED_SLASHES);
-
-            //array_push($this->test, $response);
-            //var_dump($index);
-            // var_dump($this->element["status1"]);
-            //var_dump($this->element["status2"]);
+            $this->row["details_page_url"]= self::DOMAIN . $detailsPageUrl;
+            // encoding element to JSON
+            $this->singlePageResults[$index] = json_encode($this->row, JSON_UNESCAPED_SLASHES);
         });
-        return $this->result;
+        return $this->singlePageResults;
+    }
+
+    public function getAllResultsCount($crawler)
+    {
+        $paginationString = $crawler->filter('.pagination-bottom > .right-aligned > .pagination-count')->text();
+        $resultsCount = substr($paginationString, strpos($paginationString, 'of ') + 3);
+        echo 'Total results count = ' . $resultsCount . PHP_EOL;
+    }
+
+    public function checkIfSinglePage($crawler) : bool
+    {
+        return $crawler->filter('.pagination-bottom > .right-aligned > .pagination-buttons > a:last-child')->filter('.disabled')->count() > 0;
     }
 }
